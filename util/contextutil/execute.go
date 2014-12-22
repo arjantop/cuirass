a40175d096f6b4d9dea4d83d06172efb378c2418
Arjan Topolovec <arjan.top@gmail.com>
2014-12-22 16:02:23 +0100

    Added utility functions for handling of timeouts and cancellations.

---
+++ util/contextutil/execute.go
@@ -0,0 +1,20 @@
+package contextutil
+
+import "code.google.com/p/go.net/context"
+
+func Do(ctx context.Context, f func() error) error {
+	return DoWithCancel(ctx, func() {}, f)
+}
+
+func DoWithCancel(ctx context.Context, cancel func(), f func() error) error {
+	c := make(chan error, 1)
+	go func() { c <- f() }()
+	select {
+	case <-ctx.Done():
+		cancel()
+		<-c
+		return ctx.Err()
+	case err := <-c:
+		return err
+	}
+}
---
+++ util/contextutil/execute_test.go
@@ -0,0 +1,41 @@
+package contextutil_test
+
+import (
+	"errors"
+	"testing"
+	"time"
+
+	"github.com/arjantop/cuirass/util/contextutil"
+	"github.com/stretchr/testify/assert"
+
+	"code.google.com/p/go.net/context"
+)
+
+func TestDoErrorReturned(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	err := contextutil.Do(ctx, func() error {
+		return errors.New("foo")
+	})
+	assert.Equal(t, errors.New("foo"), err)
+}
+
+func TestDoWithCancelErrorReturned(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	err := contextutil.DoWithCancel(ctx, func() {}, func() error {
+		return errors.New("foo")
+	})
+	assert.Equal(t, errors.New("foo"), err)
+}
+
+func TestDoWithCancelTimeout(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Nanosecond)
+	defer cancel()
+	var called bool
+	err := contextutil.DoWithCancel(ctx, func() { called = true }, func() error {
+		time.Sleep(2 * time.Nanosecond)
+		return nil
+	})
+	assert.Equal(t, context.DeadlineExceeded, err)
+}
